50b9e9eb78ebdb2aaec32e33e4bedaf11d86f6b1
S4MIK4RIM <sami.soureh@hotmail.com>
2024-10-22 14:40:36 +0200

    samis ändring

--- GTHoppOchLek/Program.cs
+++ GTHoppOchLek/Program.cs
@@ -5,6 +5,7 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hejsan allihopa!");
-        Console.WriteLine("Viktor här!");        
+        Console.WriteLine("Viktor här!");
+        Console.WriteLine("Hej, har du nån mer GT?");
     }
 }
