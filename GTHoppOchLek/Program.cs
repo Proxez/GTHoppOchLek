128f19f1b3933e161522585547c5e3aea25a766c
Anders Bertlén <a.bertlen@outlook.com>
2024-10-22 15:09:26 +0200

    GF

--- GTHoppOchLek/Program.cs
+++ GTHoppOchLek/Program.cs
@@ -7,6 +7,7 @@ internal class Program
         Console.WriteLine("Hejsan allihopa!");
         Console.WriteLine("Viktor här!");
         Console.WriteLine("Anders här!");
+        Console.WriteLine("Anders Branch");
 
 
 
