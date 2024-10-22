b00e93ee812d59928a43ae0c1ffa2c9518ce1a9d
Viktor Sällström <viktorsallstrom@hotmail.com>
2024-10-22 14:42:54 +0200

    update

--- GTHoppOchLek/Program.cs
+++ GTHoppOchLek/Program.cs
@@ -5,6 +5,10 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hejsan allihopa!");
-        Console.WriteLine("Viktor här!");        
+        Console.WriteLine("Viktor här!");
+
+
+
+        Console.WriteLine("Pull = hämta, Push = skicka in, fetch = 'kolla om det finns något att hämta'");
     }
 }
